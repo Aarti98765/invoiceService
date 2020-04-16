a82068db3d14b3101b8529e7b0a86af679bb4887
Aarti98765 <aartiahireengit@gmail.com>
2020-04-16 15:00:47 +0530

    ADDED: Code to given multiple rides and calculated the total for all rides

--- src/test/java/com/bridgelabz/invoiceservice/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/invoiceservice/InvoiceServiceTest.java
@@ -1,17 +1,36 @@
 package com.bridgelabz.invoiceservice;
 
 import com.bridgelabz.InvoiceGenerator;
+import com.bridgelabz.Ride;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
-    
+
     @Test
     public void givenDistanceAndTime_shouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25,fare,0.0);
+    }
+
+    @Test
+    public void givenLessDistanceOrTime_shouldReturnMinFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(5,fare,0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_shouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = {  new Ride(2.0, 5),
+                          new Ride(0.1, 1)
+                       };
+        invoiceGenerator.calculateFare(rides);
     }
 }
