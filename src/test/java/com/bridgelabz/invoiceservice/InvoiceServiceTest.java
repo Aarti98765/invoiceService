6ba5928c4a3c9414fce19cc418e51fc8172a15da
Aarti98765 <aartiahireengit@gmail.com>
2020-04-16 14:28:58 +0530

    ADDED: Code to check if minimum fare is 5

--- src/test/java/com/bridgelabz/invoiceservice/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/invoiceservice/InvoiceServiceTest.java
@@ -9,13 +9,9 @@ public class InvoiceServiceTest {
     @Test
     public void givenDistanceAndTime_shouldReturnTotalFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-        double distance = 2.0;
-        int time = 5;
+        double distance = 0.1;
+        int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
-        Assert.assertEquals(25,fare,0.0);
-    }
-
-    @Test
-    public void name() {
+        Assert.assertEquals(5,fare,0.0);
     }
 }
